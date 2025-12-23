6a3a109a33823358dd6519ed7cbd03bf53dffa63
Selin Karagullu <selin.karagullu@gmail.com>
2025-12-23 17:28:45 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { MusicPlayer } from "./components/MusicPlayer";
+import { AllSongs } from "./components/AllSongs";
+import { Playlists } from "./components/Playlists";
+
+import { BrowserRouter, Routes, Route } from "react-router";
+import { MusicProvider } from "./contexts/MusicContext";
+import { Navbar } from "./components/Navbar";
+
+
+function App() {
+
+
+  return (
+    <div className="app">
+      <main className="app-main">
+        <div className="player-section">
+
+        </div>
+        <div className="content-section">
+
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
